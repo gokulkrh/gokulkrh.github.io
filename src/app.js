ac8454a1982fb1baac57a4d68d9d8d38d01b6352
gokulkrh <gokulkrh@gmail.com>
2024-07-03 20:01:41 +0530

    initial version of ssr blog

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import SSRBlog from "./pages/SSR-Blog/ssr_blog";
 import "./App.css";
 
