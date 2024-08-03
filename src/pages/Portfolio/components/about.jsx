e4ace127b8043cea6cb59d9dfcb2f353134dd610
Gokul Krishnan G <gokulkrh@gmail.com>
2024-08-03 19:00:43 +0530

    workflow test

--- src/pages/Portfolio/components/about.jsx
+++ src/pages/Portfolio/components/about.jsx
@@ -3,7 +3,7 @@ import { React, Fragment } from "react";
 export default function AboutSection() {
   return (
     <Fragment>
-      <h1>About Mua</h1>
+      <h1>About</h1>
     </Fragment>
   );
 }
