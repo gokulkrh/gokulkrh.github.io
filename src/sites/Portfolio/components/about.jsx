d8731b02f3f36672e722acdf2bf0a401d87fc161
gokulkrh <gokulkrh@gmail.com>
2026-05-16 11:54:59 +0530

    migrate project to vite

--- src/sites/Portfolio/components/about.jsx
+++
@@ -1,9 +0,0 @@
-import { React, Fragment } from "react";
-
-export default function AboutSection() {
-  return (
-    <Fragment>
-      <h1>About</h1>
-    </Fragment>
-  );
-}
--- src/sites/Portfolio/components/work_experience.jsx
+++
@@ -1,9 +0,0 @@
-import { React, Fragment } from "react";
-
-export default function WorkExpSection() {
-  return (
-    <Fragment>
-      <h1>Work experience</h1>
-    </Fragment>
-  );
-}
